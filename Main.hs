2a2117728714c747a05458e9a9c1cf5336275dcd
Michael Tolly <miketolly@gmail.com>
2018-11-07 19:44:47 -0600

    Updates

--- Main.hs
+++ Main.hs
@@ -18,15 +18,15 @@ import XMonad.Hooks.SetWMName
 
 confirm :: String -> X () -> X ()
 confirm m f = do
-  result <- dmenu [m]
+  result <- menuArgs "dmenu" ["-p", m, "-fn", "Noto Sans CJK JP", "-l", "10"] ["y", "n"]
   when ("y" `isPrefixOf` result) f
 
 xmobarHighlight :: String
-xmobarHighlight = "#88e5fc"
+xmobarHighlight = "#c79df2"
 
 myManageHook :: ManageHook
 myManageHook = composeAll
-  [ className =? "discord" --> doShift "8discord"
+  [ className =? "discord" --> doShift "8disc"
   , className =? "Keepassx" --> doShift "9pw"
   ]
 
@@ -37,19 +37,20 @@ main :: IO ()
 main = do
   xmproc <- spawnPipe "xmobar"
   spawn "killall nautilus"
-  spawn "feh --bg-fill /home/mtolly/Pictures/1497743550594.png"
+  spawn "feh --bg-fill /home/mtolly/Pictures/1519244725109.jpg"
   xmonad $ docks $ ewmh def
     { manageHook = myManageHook <+> manageHook def
     , layoutHook = let
       tiled = Tall 1 (3/100) (3/5)
       adjust = avoidStruts
       in adjust tiled ||| noBorders Full
-    , workspaces = ["1", "2", "3", "4", "5", "6", "7", "8discord", "9pw"]
+    , workspaces = ["1", "2", "3", "4", "5", "6", "7", "8disc", "9pw"]
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
-      , ppCurrent = xmobarColor xmobarHighlight "" . wrap "{" "}"
+      , ppCurrent = xmobarColor xmobarHighlight "" . wrap "(" ")"
       , ppTitle = xmobarColor xmobarHighlight "" . shorten 100
       , ppLayout = const ""
+      , ppSep = " — "
       }
     , focusFollowsMouse = False
     , borderWidth = 2
@@ -64,16 +65,16 @@ main = do
       spawnOnce "pasystray"
       spawnOnce "redshift-gtk -l43.0481434:-89.3455351"
       spawnOnce "keepassx"
-      spawnOnce "discord-canary"
+      spawnOnce "discord"
       setWMName "LG3D"
     } `additionalKeys`
     [ ((mod1Mask .|. controlMask, xK_Delete), spawn "xscreensaver-command -lock; xset dpms force off")
-    , ((mod4Mask .|. shiftMask, xK_f), spawn "firefox-trunk")
+    , ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
     , ((mod4Mask .|. shiftMask, xK_t), spawn "thunar")
-    , ((mod4Mask .|. shiftMask, xK_Return), spawn "gnome-terminal")
-    , ((mod4Mask .|. shiftMask, xK_Home), spawn "unity-control-center")
+    , ((mod4Mask .|. shiftMask, xK_Return), spawn "xfce4-terminal")
+    , ((mod4Mask .|. shiftMask, xK_Home), spawn "xfce4-settings-manager")
     , ((mod4Mask, xK_p), spawn $ "rofi -show drun " ++ rofiOptions)
     , ((mod4Mask, xK_g), spawn $ "rofi -show run " ++ rofiOptions)
-    , ((mod4Mask .|. shiftMask, xK_q), confirm "Exit" $ liftIO $ exitWith ExitSuccess)
+    , ((mod4Mask .|. shiftMask, xK_q), confirm "Logout?" $ liftIO $ exitWith ExitSuccess)
     , ((mod4Mask, xK_q), restart "xmonad" True)
     ]
